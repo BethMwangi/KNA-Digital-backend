544df20ae7eed44d3ae5601203b0940985c083fe
wanjikumwangib@gmail.com <wanjikumwangib@gmail.com>
2026-07-22 12:10:11 +0300

    update logo and verification email link

--- config/settings/base.py
+++ config/settings/base.py
@@ -139,7 +139,7 @@ SIMPLE_JWT = {
 }
 
 SPECTACULAR_SETTINGS = {
-    "TITLE": "KNA Enterprise Digital Archive Commerce Platform API",
+    "TITLE": "Urithi Enterprise Digital Archive Commerce Platform API",
     "DESCRIPTION": "Phase One — public eCommerce platform for licensed multimedia assets.",
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
